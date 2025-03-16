a2a1aac53ddd08000fe470dbd35cbd8bcb19a99c
CleversonSilva10 <cleversonps10@hotmail.com>
2025-03-15 23:57:17 -0300

    Projetos-Atividades-Faculdade

--- 6º Semestre/Compiladores/Projeto N1/implentacao.h
+++ 6º Semestre/Compiladores/Projeto N1/implentacao.h
@@ -41,78 +41,73 @@ TInfoAtomo obter_atomo(){
     TInfoAtomo info_atomo;
     info_atomo.atomo = ERRO;
 
-    if(*entrada == '/'){
+    if (*entrada == '/'){
         info_atomo = reconhece_comentario();
         return info_atomo;
     }
 
-    while(*entrada == ' ' || *entrada == '\n' || *entrada == '\r' || *entrada == '\t'){
-        if(*entrada == '\n'){
-            // Contagem de Linhas
-            contalinhas++;
-        }else if(*entrada == '\0'){
-            info_atomo.atomo = EOS;
-        }
-    }
-
-    // RECONHECER TOKEN
-
     if (isdigit(*entrada)){
         info_atomo = reconhecer_num();
     }
-    else if (islower(*entrada)){
+    else if (isalpha(*entrada) || *entrada == '_'){
         info_atomo = reconhecer_id();
+    }
+    
+    while(*entrada == ' ' || *entrada == '\n' || *entrada == '\r' || *entrada == '\t'){
+        entrada++;
+        if(*entrada == '\0'){
+            info_atomo.atomo = EOS;
+            return info_atomo;
+        }
+        if(*entrada == '\n'){
+            contalinhas++;
+        }
     }
 
     info_atomo.linha = contalinhas;
-    
     return info_atomo;
 }
 
-TInfoAtomo reconhece_comentario(){
+TInfoAtomo reconhece_comentario() {
     TInfoAtomo info_comentario;
-    info_comentario.atomo= ERRO;
-    
-    if(*entrada == '/' && *(entrada+1) == '/'){
-        entrada += 2; //APONTAR PARA INICIO DO POSSIVEL COMENTARIO
-        while (*entrada != '\n'){
-            entrada++; //DENTRO DO COMENTARIO
+    info_comentario.atomo = ERRO;
+
+    if((*entrada == '/' && *(entrada+1) == '/') || (*entrada == '/' && *(entrada+1) == '*')){
+        entrada += 2; // Dentro do Comentario
+        while ((*entrada != '/' && *(entrada+1) != '/') || (*entrada != '/' && *(entrada+1) != '/')){
+            if(*entrada == '\n'){
+                contalinhas++;
+            }else if (*entrada == '\0'){
+                return info_comentario;
+            }
+            entrada++;
         }
-            info_comentario.atomo = COMENTARIO;
     }
 
-    if(*entrada == '/' && *(entrada+1) == '*'){
-        entrada += 2; //APONTAR PARA INICIO DO POSSIVEL COMENTARIO
-        while (*entrada != '*' && *entrada != '/'){
-            entrada++; //DENTRO DO COMENTARIO
-        }
-            info_comentario.atomo = COMENTARIO;
-    }
-    
+    entrada += 2;
+    info_comentario.linha = contalinhas;
+    info_comentario.atomo = COMENTARIO;
     return info_comentario;
 }
-    
+
+
 TInfoAtomo reconhecer_id(){
     TInfoAtomo info_id;
     info_id.atomo = ERRO;
-    int cont_caracteres_id = 0;
+    int i = 0;  // Índice para armazenar letras em atributo_ID
 
-    if(isupper(*entrada) || islower(*entrada) || *entrada == '_'){
-        entrada++;
-        cont_caracteres_id++;
+    while (*entrada != '\0' && *entrada != ' ' && *entrada != '\n') {
+        if (!isalpha(*entrada) && !isdigit(*entrada) && *entrada != '_') {
+            return info_id;
     }
-
-    while ((*entrada == '\0')){
-        if(isupper(*entrada) || islower(*entrada) || *entrada == '_' || isdigit(*entrada)){
-            entrada++;
-            cont_caracteres_id++;
-            if(cont_caracteres_id > 15){
-                info_id.atomo = ERRO;
-                return info_id;
-            }
+        if (i < 16) {  // Certifique-se de não ultrapassar o tamanho do array
+            info_id.atributo_ID[i] = *entrada;  // Armazena o caractere em atributo_ID
+            i++;
         }
+        entrada++;
     }
 
+    info_id.atributo_ID[i] = '\0';  // Finaliza a string com o caractere nulo
     info_id.atomo = IDENTIFICADOR;
     return info_id;
 }
@@ -120,39 +115,17 @@ TInfoAtomo reconhecer_id(){
 TInfoAtomo reconhecer_num(){
     TInfoAtomo info_num;
     info_num.atomo = ERRO;
-    char str_num[20];
-    char *inicio_num = entrada;
-//Q0
-    if(isdigit(*entrada)){
+    char temp[50];
+    int i = 0; 
+
+    while(isdigit(*entrada) || *entrada == '.'){ 
+        temp[i] = *entrada; 
+        i++;
         entrada++;
-        goto Q1;
     }
-    return info_num;
-Q1:
-    if(isdigit(*entrada)){
-        entrada++;
-        goto Q1;
-    }else if (*entrada == '.'){
-        entrada++;
-        goto Q2;
-    }
-    return info_num;
-Q2:
-    if(isdigit(*entrada)){
-        entrada++;
-        goto Q3;
-    }
-    return info_num;
-Q3:
-    if(isdigit(*entrada)){
-        entrada++;
-        goto Q3;
-    }else if(isalpha(*entrada)){
-        return info_num;
-    }
+    
+    temp[i] = '\0';
+    info_num.atributo_numero = atof(temp);
     info_num.atomo = NUMERO;
-    strncpy(str_num, inicio_num, entrada - inicio_num);
-    str_num[entrada - inicio_num] = '\0';  
-    info_num.atributo_numero = atof(str_num);
     return info_num;
 }
--- 6º Semestre/Compiladores/Projeto N1/main.c
+++ 6º Semestre/Compiladores/Projeto N1/main.c
@@ -4,22 +4,30 @@
 // NUMERO -> DIGITO+.DIGITO+
 
 // gcc main.c -Wall -Og -g -o MiniAnalisadorLexico
-// ./MiniAnalisadorLexico
-////
+
 int main(){
     Aluno();
-
-    entrada = lerArquivo("arquivo_teste.txt");    
-    
-    //entrada = "/*asdasd*/\n//asdasd";
     TInfoAtomo info_atomo;
 
-    printf("\nIniciando leitura do arquivo...\n\n");
+    //entrada = lerArquivo("arquivo_teste.txt");    
+    //entrada = "12.4";
+    entrada = "var1 _var1 _b1ar _var2 _var3 _var4 _var5 _var6 12.4 12.4 12.4 12.4 12.4//aaabbbaa//\n\n/*aaaa*/";
+    //entrada = "12.4 12.4 12.4 12.4 12.4";
 
+    printf("\nIniciando leitura do arquivo...\n");
     do{ 
-        info_atomo = obter_atomo();
-        printf("%03d# %s | \n", info_atomo.linha,strAtomo[info_atomo.atomo]);
+        info_atomo = obter_atomo(); // Obter_atomo == Analisador Lexico
+        if(info_atomo.atomo == IDENTIFICADOR)
+            printf("\n%03d# %s | %s", info_atomo.linha, strAtomo[info_atomo.atomo], info_atomo.atributo_ID);
+
+        if(info_atomo.atomo == NUMERO)
+            printf("\n%03d# %s | %f", info_atomo.linha, strAtomo[info_atomo.atomo], info_atomo.atributo_numero);
+
+        if(info_atomo.atomo == COMENTARIO)
+        printf("\n%03d# %s", info_atomo.linha, strAtomo[info_atomo.atomo]);
+    
     }while(info_atomo.atomo != ERRO && info_atomo.atomo != EOS);
         printf("\nFim da analise lexica!\n\n");
+        
     return 0;
 }
